aba966e3ead4541312c7fb3a433525009a8fc26c
PhuongDan <dinhphuongdan@gmail.com>
2024-04-01 17:17:18 +0700

    merge

--- src/main/java/com/example/healthcare_api/controllers/DepartmentController.java
+++ src/main/java/com/example/healthcare_api/controllers/DepartmentController.java
@@ -6,12 +6,15 @@ import com.example.healthcare_api.entities.Department;
 import com.example.healthcare_api.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v3/departments")
 public class DepartmentController {
+
+
     @Autowired
     private DepartmentService departmentService;
 
@@ -20,14 +23,22 @@ public class DepartmentController {
         return departmentService.getAll();
     }
 
-    @PostMapping()
-    public Department createDepartment(@RequestBody DepartmentDTO request){
-        return departmentService.createDepartment(request);
+    @PostMapping(consumes = {"multipart/form-data"})
+    public Department createDepartment(@RequestParam("name") String name,
+                                       @RequestParam("expense") Double expense,
+                                       @RequestParam("description") String description,
+                                       @RequestParam("thumbnail") MultipartFile file) {
+        DepartmentDTO request = new DepartmentDTO();
+        request.setName(name);
+        request.setExpense(expense);
+        request.setDescription(description);
+
+        return departmentService.createDepartment(request, file);
     }
 
     @PutMapping("/{id}")
-    public Department updateDepartment(@PathVariable Long id, @RequestBody DepartmentDTO request){
-        return departmentService.updateDepartment(id,request);
+    public Department updateDepartment(@PathVariable Long id, @RequestBody DepartmentDTO request, @RequestParam(value = "file", required = false) MultipartFile file) {
+        return departmentService.updateDepartment(id, request, file);
     }
 
     @DeleteMapping("/{id}")
